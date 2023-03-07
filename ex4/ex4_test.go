de4ca28cf3636a424377f65df9eb86cba28dfc05
Vitoria Xavier <vitoria.xavier@g.globo>
2023-03-07 09:18:12 -0300

    Utilizando o pacote assert para realizar os testes

--- ex4/ex4_test.go
+++ ex4/ex4_test.go
@@ -1,48 +1,38 @@
 package ex4
 
-import "testing"
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
 
 func TestMultiplicacao ( t * testing.T) {
 
 	multiplicação := CalculadoraMultiplicacao(2, 3)
+	multiplicaçãoEsperada := 6
 
-	if multiplicação != 6 {
-		t.Error("Erro na multiplicação de 2 * 3")
-	}
-
+	assert.Equal(t, multiplicação, multiplicaçãoEsperada)
 
 	mult := CalculadoraMultiplicacao(2,2)
-	 
-	if mult != 4 {
-		t.Error("Erro na multiplicação de 2 * 2")
-	}
+	multiplicaçãoEsperada = 4
 
+	assert.Equal(t, mult, multiplicaçãoEsperada)
 }
 
 func TestDivisao ( t * testing.T) {
-
 	divisao, _ := CalculadoraDivisao(10,2)
+	divisaoEsperada := 5
 
-	if divisao != 5 {
-		t.Error("Erro na divisão de 10 / 2")
-	}
+	assert.Equal(t, divisao,divisaoEsperada)
 
 	div, _ := CalculadoraDivisao(30, 3)
 
-	if div != 10 {
-		t.Error("Erro na divisão de 30 / 3")
-	}
+	divisaoEsperada = 10
+	assert.Equal(t, div, divisaoEsperada)
 
 }
 	func TestDiv0(t * testing.T) {
-		
-		div, err := CalculadoraDivisao(0,2)
+		_ , err := CalculadoraDivisao(0,2)
 
-		if div != 5 {
-			t.Error("Erro! Não pode haver divisor ou dividendos = 0", err)
-		
-		}
-		
-		
+		assert.NotNil(t, err)
+		assert.Equal(t, ZeroOperationError, err)
 	}
-	
