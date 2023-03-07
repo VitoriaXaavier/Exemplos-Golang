aa84c9ebc1300c6f1084d8087c2eda45b02b99ac
Vitoria Xavier <vitoria.xavier@g.globo>
2023-03-07 09:18:02 -0300

    Utilizando o pacote assert para realizar os testes

--- ex4/calculadora.go
+++ ex4/calculadora.go
@@ -1,9 +1,11 @@
 package ex4
 
 import (
-
+	"errors"
 	"fmt"
 )
+
+var ZeroOperationError = errors.New("divisor ou dividendo não podem ser 0")
 
 func CalculadoraMultiplicacao (fator1 int, fator2 int) int {
 
@@ -27,17 +29,17 @@ func CalculadoraDivisao (dividendo, divisor int) (int, error) {
 	fmt.Println("Entre com o divisor")
 	fmt.Scan(&divisor)
 	
-	var err error
+
 	var quociente int
 
-	if dividendo == 0 && divisor == 0 {
-		panic(err.Error())
+	if dividendo == 0 || divisor == 0 {
+		return 0, ZeroOperationError
 	
 	}else { 
 		quociente = dividendo / divisor
 		fmt.Println("O resultado da divisão de ", dividendo, " / ", divisor, " = ", quociente)
 	}
 
-	return quociente, err
+	return quociente, nil
 
 }
