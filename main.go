aa37a28d03939f97a41fa3b61cc0bee44900b96b
Vitoria Xavier <vitoria.xavier@g.globo>
2023-03-07 09:19:00 -0300

    Adicionando outros pacotes no método main

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"github.com/VitoriaXaavier/Exemplos-Golang/ex1"
-	"github.com/VitoriaXaavier/Exemplos-Golang/ex2"
-	"github.com/VitoriaXaavier/Exemplos-Golang/ex3"
-	"github.com/VitoriaXaavier/Exemplos-Golang/ex4"
+		"github.com/VitoriaXaavier/Exemplos-Golang/ex1"
+		"github.com/VitoriaXaavier/Exemplos-Golang/ex2"
+		"github.com/VitoriaXaavier/Exemplos-Golang/ex3"
+		"github.com/VitoriaXaavier/Exemplos-Golang/ex4"
+	 	"github.com/VitoriaXaavier/Exemplos-Golang/ex5"
+		"github.com/VitoriaXaavier/Exemplos-Golang/ex6"
 )
+
 func main() {
 
 ex1.Area(1,1)
@@ -13,5 +16,6 @@ ex2.Caneta(1,1,1)
 ex3.MaximoDivisor(1,1)
 ex4.CalculadoraMultiplicacao(1,1)
 ex4.CalculadoraDivisao(1,1)
-
+ex5.ForcaExercida(1,1,1)
+ex6.EquacaoSegundoGrau(1,-1,-12)
 }
